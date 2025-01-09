7e7708ca1b83fd8b342a2262a2052150ac687846
edumoro412 <edumoro2003@gmail.com>
2025-01-09 09:21:25 +0100

    Añadimos comentarios explicando que hace cada cosa

--- LitroGym/app/routes/Registro.tsx
+++ LitroGym/app/routes/Registro.tsx
@@ -5,17 +5,20 @@ import { ActionFunction, json } from "@remix-run/node";
 import { createUser } from "~/services/user.services";
 
 export const action: ActionFunction = async ({ request }) => {
+  //Basicamente obtiene los datos del formulario
   const formData = await request.formData();
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
   try {
+    //Aqui se crea al usuario
+
     const user = await createUser(name, email, password);
     if (!user) {
       throw new Error("No se pudo crear el usuario."); // Lanza un error si `user` es null o undefined.
     }
-    return redirect("/login"); // Verifica que esta línea no tenga problemas.
+    return redirect("/login");
   } catch (error) {
     console.error("Error general:", error); // Esto ayudará a identificar errores generales.
     return json({ success: false, error: "Error al crear el usuario" });
@@ -32,6 +35,7 @@ export default function Registro() {
     error?: string;
   }>();
 
+  //El fetcher envia los datos del formulario al /registro, para que  se haga el action. Si quisiesemos usar un action en otro fichero, pondriamos la ruta de ese fichero
   const handleRegistro = (e: React.FormEvent) => {
     e.preventDefault();
     fetcher.submit(
@@ -85,6 +89,7 @@ export default function Registro() {
             Enviar
           </button>
         </form>
+
         {fetcher.data && fetcher.data.success && fetcher.data.user && (
           <p>Usuario creado correctamente: {fetcher.data.user.name}</p>
         )}
@@ -94,4 +99,6 @@ export default function Registro() {
       </div>
     </div>
   );
+
+  //El fetcher.data es la respuesta que se obtiene del action, devuelve principalmente dos parametros. El parametro succes que es true or false, y el parámetro user, que contiene todos los parametros del usuario creado. En este caso seria user{name:____, email:_____, contraseña:_____}. Si fetcher devolviese un error tambien devolveria un parametro error.
 }
