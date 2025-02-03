56d2b054b3e64a04bb2d0dae1f7d98cda74142c3
edumoro412 <edumoro2003@gmail.com>
2025-02-03 18:28:50 +0100

    Añadimos el Form de remix para evitar la recarga de la página

--- LitroGym/app/routes/Ejercicios.tsx
+++ LitroGym/app/routes/Ejercicios.tsx
@@ -1,6 +1,6 @@
 import { Ejercicio } from "@prisma/client";
 import { json, LoaderFunction, ActionFunction } from "@remix-run/node";
-import { useLoaderData, useSearchParams } from "@remix-run/react";
+import { Form, useLoaderData, useSearchParams } from "@remix-run/react";
 import { useState } from "react";
 // eslint-disable-next-line import/no-unresolved
 import { todosEjercicios } from "~/models/ejercicio.server";
@@ -98,7 +98,7 @@ export default function Ejercicios() {
 
   return (
     <div className="bg-custom-color flex flex-col items-center justify-start min-h-screen ">
-      <form className="flex border-2 my-4 border-gray-300 rounded-md mt-2 focus-within:border-blue-600 md:w-80 mx-auto">
+      <Form className="flex border-2 my-4 border-gray-300 rounded-md mt-2 focus-within:border-blue-600 md:w-80 mx-auto">
         <button className="px-2">
           <SearchIcon />
         </button>
@@ -110,7 +110,7 @@ export default function Ejercicios() {
           defaultValue={searchParams.get("search") ?? ""}
           className="w-full py-3 px-2 outline-none"
         />
-      </form>
+      </Form>
 
       <ul className="flex flex-wrap justify-center gap-8 overflow-x-auto snap-x snap-mandatory md:snap-none">
         {data.map((ejercicio: Ejercicio) => (
