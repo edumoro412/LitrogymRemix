118c6826a80bf8b261f6190b06acf11acc8b5cf2
pitumola <marcoslgl2004@gmail.com>
2025-02-04 12:17:36 +0100

    modificado el diseño y el fondo de alimentacion y modificado el escalado de las imagenes de home

--- LitroGym/app/routes/Alimentacion._index.tsx
+++ LitroGym/app/routes/Alimentacion._index.tsx
@@ -3,16 +3,27 @@ export default function Alimentacion() {
   return (
     <>
       <div className="alimentacion">
-        <b className="alimentacion-titulo">ALIMENTACION</b>
         <div className="alimentacion-contenedor">
+          <b className="alimentacion-titulo">RECETAS</b>
           <div className="alimentacion-contenedor-contenido">
-            <p id="titulo">Recetas:</p>
-            <Link to="Receta1"><button>· Recetas con pechuga de pollo o pavo</button></Link>
-            <Link to="Receta2"><button>· Recetas con otras carnes o queso</button></Link>
-            <Link to="Receta3"><button>· Recetas con pescado</button></Link>
-            <Link to="Receta4"><button>· Recetas veganas</button></Link>
-            <Link to="Receta5"><button>· Recetas de postres proteicos</button></Link>
-            <Link to="Receta6"><button>· Recetas en airfryer</button></Link>
+            <Link to="Receta1">
+              <button>· Recetas con pechuga de pollo o pavo</button>
+            </Link>
+            <Link to="Receta2">
+              <button>· Recetas con otras carnes o queso</button>
+            </Link>
+            <Link to="Receta3">
+              <button>· Recetas con pescado</button>
+            </Link>
+            <Link to="Receta4">
+              <button>· Recetas veganas</button>
+            </Link>
+            <Link to="Receta5">
+              <button>· Recetas de postres proteicos</button>
+            </Link>
+            <Link to="Receta6">
+              <button>· Recetas en airfryer</button>
+            </Link>
           </div>
         </div>
       </div>
